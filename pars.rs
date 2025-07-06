19532ce73d50911d0f1fd8953c77d56f49c9408f
Simar Malhotra <malhotrasimar009@gmail.com>
2025-07-05 20:02:45 -0400

    checks each line O(n) where n=fn_names.len(); need to improve

--- pars.rs
+++ pars.rs
@@ -1,7 +1,6 @@
 use std::fs;
-use std::env;
-
 use clap::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 struct Cli {
@@ -9,21 +8,41 @@ struct Cli {
 }
 
 fn main() {
-    let args= Cli::parse();
+    let args = Cli::parse();
 
     let file_path = args.file_path;
     let file_contents = fs::read_to_string(file_path).expect("Unable to read file");
 
-    let mut lines = file_contents.lines().peekable();
+    let lines: Vec<&str> = file_contents.lines().collect();
 
-    while let Some(line) = lines.next() {
+    let mut hm: HashMap<String, Vec<String>> = HashMap::new();
+    let mut fn_names: Vec<String> = Vec::new();
+
+    let mut i = 0;
+    while i < lines.len() {
+        let line = lines[i];
         if line.trim_start().starts_with("def") {
             let mut local_line = line.to_string();
+            let fn_name = line
+                .trim_start()
+                .trim_start_matches("def")
+                .trim()
+                .split('(')
+                .next()
+                .unwrap_or("")
+                .trim()
+                .to_string();
+
+            if !hm.contains_key(&fn_name) {
+                hm.insert(fn_name.clone(), Vec::new());
+                fn_names.push(fn_name.clone());
+            }
 
             while !local_line.trim_end().ends_with(':') {
-                if let Some(next_line) = lines.next() {
+                i += 1;
+                if i < lines.len() {
                     local_line.push_str(" ");
-                    local_line.push_str(next_line.trim());
+                    local_line.push_str(lines[i].trim());
                 } else {
                     break;
                 }
@@ -31,5 +50,45 @@ fn main() {
 
             println!("[FUNC]: {}", local_line);
         }
+        i += 1;
     }
+
+    let mut i = 0;
+    let mut current_fn: Option<String> = None;
+
+    while i < lines.len() {
+        let line = lines[i];
+
+        if line.trim_start().starts_with("def") {
+            let fn_name = line
+                .trim_start()
+                .trim_start_matches("def")
+                .trim()
+                .split('(')
+                .next()
+                .unwrap_or("")
+                .trim()
+                .to_string();
+
+            current_fn = Some(fn_name.clone());
+            i += 1;
+            continue;
+        }
+
+        if let Some(curr) = &current_fn {
+            for fname in &fn_names {
+                if line.contains(fname) && fname != curr {
+                    if let Some(callees) = hm.get_mut(curr) {
+                        if !callees.contains(fname) {
+                            callees.push(fname.to_string());
+                        }
+                    }
+                }
+            }
+        }
+
+        i += 1;
+    }
+
+    println!("{:#?}", hm);
 }
