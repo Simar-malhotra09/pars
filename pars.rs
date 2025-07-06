9ea367e28a4f22a2a9c9dd5dfff488e162c07c16
Simar Malhotra <malhotrasimar009@gmail.com>
2025-07-06 15:28:00 -0400

    reading line by line is not it, exploring other options

--- pars.rs
+++ pars.rs
@@ -8,16 +8,39 @@ struct Cli {
 }
 
 fn main() {
+    // get the file path & open file as a string; Need to look at alternatives! 
     let args = Cli::parse();
-
     let file_path = args.file_path;
     let file_contents = fs::read_to_string(file_path).expect("Unable to read file");
-
+    
+    // store each line in a vector
+    // I was thinking to somehow parallelize this,
+    // each thread looks at subset. I need to check what rust offers though first. 
     let lines: Vec<&str> = file_contents.lines().collect();
-
+    
+    // I keep a hashMap with each unique function name(parent_fn) (doesn't include args)
+    // as key and a vector as values which stores other functions(child_fns) called 
+    // in the parent_fn's scope
     let mut hm: HashMap<String, Vec<String>> = HashMap::new();
     let mut fn_names: Vec<String> = Vec::new();
 
+    // The following approach is pretty lanky! 
+
+    // First pass of the file: 
+    // If line starts with 'def'; I can't think of a case where this doesn't happen when defining a
+    // fn. We store the fn_name in the hashMap if not already in it, and init an empty vector. 
+    //
+    // 1 of 2 things can happen then:
+    //
+    // 1. function definition ends on the same line => def add(a:int, b:int)-> c:int:
+    //
+    // 2. function definition ends on any line other than the same=>
+    // def add(
+    // a:int,
+    // b:int, 
+    // )->
+    // c:int:
+    //
     let mut i = 0;
     while i < lines.len() {
         let line = lines[i];
@@ -37,6 +60,7 @@ fn main() {
                 hm.insert(fn_name.clone(), Vec::new());
                 fn_names.push(fn_name.clone());
             }
+            let mut fn_end: bool = false;
 
             while !local_line.trim_end().ends_with(':') {
                 i += 1;
@@ -48,7 +72,7 @@ fn main() {
                 }
             }
 
-            println!("[FUNC]: {}", local_line);
+            // println!("[FUNC]: {}", local_line);
         }
         i += 1;
     }
