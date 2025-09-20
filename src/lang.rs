5ff0ef053807367dbc7756295162104c1e07629d
Simar Malhotra <malhotrasimar009@gmail.com>
2025-09-20 00:47:47 -0400

    store func signature def and other things to support parsing multiple file types

---
+++ src/lang.rs
@@ -0,0 +1,12 @@
+pub mod python {
+    pub const FUNC_DEF: &str = "def";
+    pub const PARAMS: &str = "()";
+    pub const END_DEF: &str = ":";
+}
+
+pub mod rust {
+    pub const FUNC_DEF: &str = "fn";
+    pub const PARAMS: &str = "()";
+    pub const END_DEF: &str = "{";
+}
+
